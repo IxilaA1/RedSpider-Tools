4042eb29e26d1b3a0b4e4c212df5bd017b6d7a6a
IxilA1 <vadimtchepikov@gmail.com>
2025-11-22 14:13:34 +0100

    Delete RED_SPIDER directory

--- RED_SPIDER/files/fqfzq.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import time
-print("Lancement du payload BadUSB...")
-print("Attente de l'initialisation...")
-time.sleep(1) # Simule une attente brève
-print("✅ Injection réussie! (Test d'encodage: accentué)")
